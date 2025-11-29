717692f5af037572777a69b75f13319e0e9b9ed5
Aryan <ak1544871@gmail.com>
2025-11-29 18:46:30 +0530

    corrected getAllShows Controller

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -41,7 +41,7 @@ export const getDashboardData = async (req, res)=>{
 //API to get all shows
 export const getAllShows = async (req, res)=>{
     try {
-         const shows = (await Show.find({showDateTime: {$gte: new Date()}}).populate('movie')).sort({showDateTime:1});
+         const shows = await Show.find({showDateTime: {$gte: new Date()}}).populate('movie').sort({showDateTime:1});
          res.json({success: true, shows})
     } catch (error) {
         console.error(error);
