04cbdb8999319b89cb24603a4a3e315664a338ac
Aryan <ak1544871@gmail.com>
2025-11-30 14:37:06 +0530

    added stripe payments in backend

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,5 +1,6 @@
 import Booking from "../models/Booking.js";
 import Show from "../models/Show.js";
+import stripe from 'stripe';
 
 // Function to check availability of seats for a movie
 const checkSeatAvailability = async (showId, selectedSeats) => {
@@ -41,8 +42,36 @@ export const createBooking = async (req, res) =>{
         });
         showData.markModified('occupiedSeats');
         await showData.save();
+
         // Stripe Gatway Initialize
-        res.json({success: true, message: 'Booking created successfully'});
+        const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
+        //Creating line items for Stripe
+        const line_items = [{
+            price_data:{
+                currency: 'usd',
+                product_data: {
+                    name : showData.movie.title,
+                },
+                unit_amount:Math.floor(booking.amount) * 100
+            },
+            quantity: 1,
+        }];
+
+        const session = await stripeInstance.checkout.sessions.create({
+            success_url:`${origin}/loading/my-bookings`,
+            cancel_url: `${origin}/my-bookings`,
+            line_items: line_items,
+            mode: 'payment',
+            metadata: {
+                bookingId: booking._id.toString(),
+            },
+            expires_at: Math.floor(Date.now() / 1000) + 30 * 60, // 30 minutes expiration
+        })
+
+        booking.paymentLink = session.url;
+        await booking.save();
+
+        res.json({success: true, url: session.url});
     } catch (error) {
         console.error(error);
         res.json({success: false, message: error.message})
